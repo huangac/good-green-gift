64bc75972455ec99cdbd6b7a2f6a2435e10df77a
huangac <huangac@usc.edu>
2023-04-28 11:02:25 -0700

    implemented localStorage to shop page

--- shop.js
+++ shop.js
@@ -4,11 +4,16 @@ let total = 0;
 let price = 24.99;
 
 function calcCart(qty) {
-    cartQty += parseFloat(qty);
+    cartQty = parseFloat(cartQty) + parseFloat(qty);
     subtotal = parseFloat(cartQty) * parseFloat(price);
-    total = (subtotal * 1.0925).toFixed(2)
-    $("#view-cart-button").val("View cart (" + cartQty + ")");
-
+    total = (subtotal * 1.0925).toFixed(2);
+    localStorage.setItem("cartQty", cartQty);
+    if (cartQty > 0) {
+        $("#view-cart-button").val("View cart (" + cartQty + ")");
+    } else {
+        $("#view-cart-button").val("View cart");
+    }
+    
 }
 function viewCart() {
     if (cartQty != 0) {
@@ -16,11 +21,14 @@ function viewCart() {
         var subtotalHTML = "<p> Subtotal: <strong>" + subtotal + "</strong></p>";
         var totalHTML =  "<p> Total: <strong>" + total + "</strong></p>";
         $("#cart-info").html(entryHTML + '<div id="cost">' + subtotalHTML + totalHTML + '</div>');
-        console.log("cart has stuff");
+        //console.log("cart has stuff");
+    } else {
+        $("#cart-info").html("Currently empty :(");
+
     }
     $("#cart-container").css("opacity", 1);
     $("#cart-container").css("z-index", 999);
-    console.log("cart qty: " + cartQty + " subtotal: " + subtotal);
+    //console.log("cart qty: " + cartQty + " subtotal: " + subtotal);
 }
 
 function closeCart() {
@@ -28,26 +36,31 @@ function closeCart() {
     $("#cart-container").css("z-index", -1);
 }
 
-$("#cart-container").on("click", "#remove-button", function() {
-    cartQty = 0;
-    calcCart();
-    viewCart();
-    console.log("remove");
-})
-
 $(document).ready(function() {
+    console.log(localStorage.getItem("cartQty"));
+    if (localStorage.getItem("cartQty") > 0) {
+        cartQty = localStorage.getItem("cartQty");
+        calcCart(0);
+    }
     $("#add-to-cart-button").on("click", function() {
         var quantity = $("#quantity").val();
         calcCart(quantity);
-        console.log("add to cart");
+        //console.log("add to cart");
     });
 
     $("#view-cart-button").on("click", function() {
         viewCart();
-        console.log("view cart");
+        //console.log("view cart");
     });
 
     $("#cancel-cart-button").on("click", function() {
         closeCart();
+    })
+    $("#cart-container").on("click", "#remove-button", function() {
+        //console.log("remove");
+        cartQty = 0;
+        calcCart(0);
+        viewCart();
+        localStorage.clear();
     })
 });
